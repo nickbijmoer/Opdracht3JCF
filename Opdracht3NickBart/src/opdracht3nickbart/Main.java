4ea7d4dc359f44d94e33c6b15f09fbb9da12b0cd
nickbijmoer <nick@bijmoer.nl>
2016-03-29 19:36:10 +0200

    working on it

---
+++ Opdracht3NickBart/src/opdracht3nickbart/Main.java
@@ -0,0 +1,19 @@
+package opdracht3nickbart;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        TableViewSample c = new TableViewSample();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
