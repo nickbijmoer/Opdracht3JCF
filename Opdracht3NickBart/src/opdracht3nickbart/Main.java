9c56f98a761f92515b75d7486797a5271a37e967
nickbijmoer <nick@bijmoer.nl>
2016-03-30 11:42:43 +0200

    kek

--- Opdracht3NickBart/src/opdracht3nickbart/Main.java
+++ Opdracht3NickBart/src/opdracht3nickbart/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //TableViewSample c = new TableViewSample();
+        TableViewSample c = new TableViewSample();
         TreeViewSample tree = new TreeViewSample();
     }
 
